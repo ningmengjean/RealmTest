4933b6267a5615d2f3ab60e31c0c3f3ff6137321
zhu xiaojin <woshizhuxiaojin@sina.com>
2021-08-15 18:42:43 +0800

    Update MockMessages.swift

--- RealmTest/MockMessages.swift
+++ RealmTest/MockMessages.swift
@@ -9,11 +9,16 @@ import Foundation
 import RealmSwift
 
 class MockMessages: NSObject {
-    let user4 = User(userName: "user4", email: "user4@localhost", displayName: "user5", avatarImage: nil)
+    let user4 = User(userName: "user4", email: "user4@localhost", displayName: "user4", avatarImage: nil)
     let user5 = User(userName: "user5", email: "user5@localhost", displayName: "user5", avatarImage: nil)
-    let messageRoom1 = MessageRoom(displayName: "user5", timeStamp: Date(), users: [User(userName: "user4", email: "user4@localhost", displayName: "user4", avatarImage: nil),User(userName: "user5", email: "user5@localhost", displayName: "user5", avatarImage: nil)], messages: [ChatMessage(messageBody: "hello", messageKind: .Text, timeStamp: Date(), partition: "user5@localhostuser4@localhost", senderID: User(userName: "user4", email: "user4@localhost", displayName: "user4", avatarImage: nil).userName, receiverID: nil),ChatMessage(messageBody: "hellohello", messageKind: .Text, timeStamp: Date(), partition: "user5@localhostuser4@localhost", senderID: nil, receiverID: User(userName: "user5", email: "user5@localhost", displayName: "user5", avatarImage: nil).userName)])
-    let chatMessage1 = ChatMessage(messageBody: "hello", messageKind: .Text, timeStamp: Date(), partition: "user5@localhostuser4@localhost", senderID: User(userName: "user4", email: "user4@localhost", displayName: "user4", avatarImage: nil).userName, receiverID: nil)
-    let chatMessage2 = ChatMessage(messageBody: "hellohello", messageKind: .Text, timeStamp: Date(), partition: "user5@localhostuser4@localhost", senderID: nil, receiverID: User(userName: "user5", email: "user5@localhost", displayName: "user5", avatarImage: nil).userName)
+    lazy var messageRoom1: MessageRoom = {
+       return MessageRoom(displayName: "user5",
+                          timeStamp: Date(),
+                          users: [user4,user5],
+                          messages: [chatMessage1,chatMessage2])
+    }()
+    let chatMessage1 = ChatMessage(messageBody: "hello", messageKind: .Text, timeStamp: Date(), partition: "user5@localhost.user4@localhost", senderID: User(userName: "user4", email: "user4@localhost", displayName: "user4", avatarImage: nil).userName, receiverID: nil)
+    let chatMessage2 = ChatMessage(messageBody: "hellohello", messageKind: .Text, timeStamp: Date(), partition: "user5@localhost.user4@localhost", senderID: nil, receiverID: User(userName: "user5", email: "user5@localhost", displayName: "user5", avatarImage: nil).userName)
     func saveData() {
         RealmService.shared.saveObject(user4)
         RealmService.shared.saveObject(user5)
@@ -21,6 +26,13 @@ class MockMessages: NSObject {
         RealmService.shared.saveObject(chatMessage1)
         RealmService.shared.saveObject(chatMessage1)
     }
+    
+//    private func generateRandomMessage(roomId: String) -> ChatMessage {
+//        let ids = roomId.split(separator: ".")
+//        let user1 = ids.first ?? ""
+//        let user2 = ids.last ?? ""
+//
+//    }
     
     private func messageRoomID(users: [User]) -> String {
         var id = String()
@@ -37,4 +49,3 @@ class MockMessages: NSObject {
     
     
 }
-
